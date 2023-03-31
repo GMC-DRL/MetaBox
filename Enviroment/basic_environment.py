b284cbf5aa7b5dfa22939d1e3197d155839c8350
Anchorrrr <chenjui.lee@gmail.com>
2023-03-31 20:42:38 +0800

    add class PBO_Env

--- Enviroment/basic_environment.py
+++ Enviroment/basic_environment.py
@@ -10,3 +10,31 @@ class Env:
 
     def step(self, population):
         return self.problem_instance.func(population)
+
+
+class PBO_Env(Env):
+    def __init__(self,
+                 problem_instance,
+                 dim,
+                 lower_bound,
+                 upper_bound,
+                 population_size,
+                 FEs):
+        Env.__init__(self, problem_instance)
+        self.dim = dim
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.population_size = population_size
+        self.FEs = FEs
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def get_feature(self):
+        pass
+
+    def get_reward(self):
+        pass
