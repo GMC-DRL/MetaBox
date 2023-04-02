a599a005c7538cee8d0ed0f11a67fc685db46e1f
LZR <chenjui.lee@gmail.com>
2023-04-02 13:05:42 +0800

    allow evaluation of individual in list

--- Problem/cec_test_func.py
+++ Problem/cec_test_func.py
@@ -57,6 +57,8 @@ class Problem:
         """
         A general version of func() with adaptation to evaluate both individual and population.
         """
+        if isinstance(x, list):
+            x = np.array(x)
         if x.ndim == 1:  # x is a single individual
             return self.func(x.reshape(1, -1))[0]
         elif x.ndim == 2:  # x is a whole population
