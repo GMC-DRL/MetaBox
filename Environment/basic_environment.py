ffba1ab7b4c1af0b122a0218fea4646a5d7d8f5a
LZR <chenjui.lee@gmail.com>
2023-04-07 22:55:20 +0800

    bugs fixed

--- Environment/basic_environment.py
+++ Environment/basic_environment.py
@@ -22,12 +22,12 @@ class PBO_Env(Env):
         self.optimizer.init_population(self.problem)
 
     def step(self, action):
-        parent_cost = self.optimizer.cost
+        parent_cost = self.optimizer.cost.copy()
         self.optimizer.evolve(self.problem, action)
 
         state = {'population': self.optimizer.population,
                  'cost': self.optimizer.cost,
                  'fes': self.optimizer.fes}
         reward = self.reward_func(cur=self.optimizer.cost, parent=parent_cost, init=self.optimizer.init_cost)
-        is_done = self.optimizer.fes >= self.optimizer.FEs or self.optimizer.cost.min() <= 1e-8
+        is_done = self.optimizer.fes >= self.optimizer.maxFEs or self.optimizer.cost.min() <= 1e-8
         return state, reward, is_done
